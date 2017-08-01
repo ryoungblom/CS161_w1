e9c1ed58e96f7edb68c5e8d410942431b11aa8b9
ry <rjyoungblom@gmail.com>
2017-07-31 22:41:39 -0700

    wk_1
    week one code: prompts user to enter favorite animal and prints is back out.

---
+++ animal.cpp
@@ -0,0 +1,20 @@
+/************************
+** Author: Reuben Youngblom
+** Date: October 2nd, 2016 (added class late, emailed Prof. Alcon)
+** Description: Assignment 1, favorite animal code
+************************/
+
+#include <iostream>
+#include <string>
+
+// a simple example program
+int main ()
+{
+	std::string faveAnimal;
+	std::cout << "Please enter your favorite animal." << std::endl;
+	std::cin >> faveAnimal;
+	std::cout << "Your favorite animal is the " << faveAnimal;
+	std::cout << "." << std::endl;
+
+}
+
